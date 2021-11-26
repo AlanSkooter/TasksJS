3b9a03fe2f05cd7df8af5f9779334cabee3f910a
AlanSkooter <alinasozonovich@gmail.com>
2021-11-26 22:35:13 +0300

    add: converting

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 "use strict";
 
-let user = {  
-    name: "Алина", 
-    surname: "Товмасян",
-    age: 29,
-    city: "Минск",
-    isDriver: false,       
+const user = {  
+    name: prompt('Твое имя?'), 
+    surname: prompt('Твоя фамилия?'),
+    age: Number(prompt('Сколько тебе лет?')),
+    city: prompt('Из какого ты города?'),
+    isDriver: confirm('Есть водительское удостоверение?'),       
   };
 
-console.log(`Привет, меня зовут ${user.name} ${user.surname}, мне ${user.age} лет, я живу в городе ${user.city}`);
+  if (user.isDriver == true) {
+    console.log(`Привет, меня зовут ${user.name} ${user.surname}, мне ${user.age} лет, я живу в городе ${user.city}, я вожу автомобиль`);
+  } else {
+    console.log(`Привет, меня зовут ${user.name} ${user.surname}, мне ${user.age} лет, я живу в городе ${user.city}, я не езжу за рулем`);
+  }
