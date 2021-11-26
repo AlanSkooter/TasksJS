5a7cf7036d8b7d013aca25546ddf99a27a1f83a9
AlanSkooter <alinasozonovich@gmail.com>
2021-11-26 22:30:36 +0300

    add: types

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let user = {  
+    name: "Алина", 
+    surname: "Товмасян",
+    age: 29,
+    city: "Минск",
+    isDriver: false,       
+  };
+
+console.log(`Привет, меня зовут ${user.name} ${user.surname}, мне ${user.age} лет, я живу в городе ${user.city}`);
