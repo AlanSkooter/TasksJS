e79b42de954e02bdce72a441d74ec59ed476d034
AlanSkooter <alinasozonovich@gmail.com>
2021-12-17 17:36:07 +0300

    fix: split

--- script2.js
+++ script2.js
@@ -2,3 +2,13 @@
 
 const files = ['text.docx', 'index.html', 'document.pdf', 'script.js', 'style.css', 'summary.pdf', 'Harry_Potter.pdf', 'report.pdf'];
 
+let arr = [];
+
+files.filter((elem) => {
+    let sepArr = elem.split(".");
+    if (sepArr[sepArr.length - 1] ==="pdf") {
+        arr.push(elem);
+    }
+});
+
+console.log(arr);
