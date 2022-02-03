7a13445d8992aef58c7ce3a71950c9bbc75678cf
AlanSkooter <alinasozonovich@gmail.com>
2022-02-03 20:22:03 +0300

    fix: then

--- script1.js
+++ script1.js
@@ -1,16 +1,10 @@
 'use strict';
 
-function cleanRoom() {
-  setTimeout(() => console.log("Команата убрана"), 5000);
-  }
-
-cleanRoom();
-
 const promise = new Promise(function(resolve, reject) {
   setTimeout(() => resolve(), 5000)
 });
 
 promise
     .then(() => {
-    console.log("Команата убрана")
+    console.log("Команата убрана");
     });
--- script2.js
+++ script2.js
@@ -1,12 +1,15 @@
 'use strict';
 
 function cleanRoom(dirtyLevel) {
-  if (dirtyLevel < 0 || dirtyLevel > 10){
-    console.log("От 0 до 10, дурак!");
-  } else {
     return new Promise(function(resolve, reject) {
       setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000)
-    })
-    .then(result => console.log(`Уборка проведена успешно за ${result} секунд`));
+    });
   }
-}
+
+cleanRoom(5).then((result) => {
+  if (result < 0 || result > 10){
+    console.log("Здесь слишком грязно!");
+  } else {
+  console.log(`Уборка проведена успешно за ${result} секунд`);
+  }
+});
